bb55c43c465186c7810ffe2ee6bd02ece4f1fadd
Calvin Kwee <calvink.ti@stu.untar.ac.id>
2016-12-06 19:18:25 +0700

    Update Home.java

--- Home.java
+++ Home.java
@@ -4,72 +4,81 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- * Created by Steven on 12/4/2016.
- */
 public class Home extends JFrame implements ActionListener{
     JTabbedPane jtp = new JTabbedPane();
-    JButton button_logout,button_ubahpin,button_ubahpass;
-
-
-    Home(){
+    JButton button_logout,button_ubahpin,button_ubahpass,searching;
+    JTextField searchField;
+    String emails;
+    String name;
+    Home(String mail,String nama){
+    	emails=mail;
+    	name=nama;
+    	System.out.println(emails+" "+name);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-        JPanel panel1 = new JPanel(new GridLayout(2,2,20,20));
-        JPanel panel2 = new JPanel(new GridLayout(1,3,20,20));
-        setTitle("Welcome");
+        JPanel panel2 = new JPanel(new GridLayout(1,3));
+        JPanel panel3 = new JPanel(new GridLayout(1,2));
+        setTitle("Welcome, "+name);
         setSize(800,600);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
         button_logout = new JButton("Logout");
         button_ubahpin = new JButton("Ubah PIN");
         button_ubahpass = new JButton("Ubah Password");
+        searching = new JButton("Search");
         jtp.addTab("Inbox" , new Panel1());
         jtp.addTab("OutBox" , new Panel2());
         jtp.addTab("Compose" , new Panel3());
-        panel2.add(button_logout);
+        searchField = new JTextField();
+        panel2.add(searching);
         panel2.add(button_ubahpass);
         panel2.add(button_ubahpin);
-
+        panel2.add(button_logout);
+        panel3.add(searchField,BorderLayout.WEST);
+        panel3.add(panel2,BorderLayout.EAST);
+        add(panel3);
         add(jtp);
-
-        panel1.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
-
-        panel2.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        panel3.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         //add(panel1);
-        add(panel2);
         setVisible(true);
+        button_ubahpass.addActionListener(this);
+        searching.addActionListener(this);
+        button_ubahpin.addActionListener(this);
+        button_logout.addActionListener(this);
     }
 
-    public static void main(String[] args){
-        new Home();
-    }
-    @Override
-    public void actionPerformed(ActionEvent arg0) {
-        try{
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		try{
             if(arg0.getSource()==button_logout){
                 new MainProgram();
+                System.out.println(emails+" "+name);
                 this.dispose();
             }
             else if(arg0.getSource()==button_ubahpass){
-                new Upass();
+            	System.out.println(emails+" "+name);
+                new Upass(emails,name);
+                System.out.println(emails+" "+name);
                 this.dispose();
             }
             else if(arg0.getSource()==button_ubahpin){
-                new UPIN();
+            	System.out.println(emails+" "+name);
+                new UPIN(emails,name);
                 this.dispose();
             }
         }
         catch (Exception e){
             System.out.println(e);
         }
-    }
+	}
 }
 
 class Panel1 extends JPanel {
-    public Panel1(){
-        JButton button_reply = new JButton("Reply");
+    JButton button_reply,button_delete;
+	public Panel1(){
+    	JPanel panel11 = new JPanel(new GridLayout(2,1));
+    	JPanel panel111 = new JPanel(new GridLayout(1,2));
+        button_reply = new JButton("Reply");
+        button_delete = new JButton("Delete");
         String[] colHeads = { "Name", "Extension", "ID#" };
         Object[][] data = {
                 {"Gail","456715","5921214"},
@@ -78,19 +87,24 @@ class Panel1 extends JPanel {
         };
         JTable table = new JTable(data, colHeads);
         JScrollPane jsp = new JScrollPane(table);
-        add(jsp);
-        add(button_reply);
+        panel11.add(jsp);
+        panel111.add(button_reply);
+        panel111.add(button_delete);
+        panel11.add(panel111);
+        add(panel11);
     }
 }
 
 class Panel2 extends JPanel {
-    public Panel2(){
+    JButton button_delete;
+	public Panel2(){
 //        JPanel table_panel = new JPanel();
 //        table_panel.setLayout(new BoxLayout(table_panel,BoxLayout.LINE_AXIS));
 //        JPanel button_panel = new JPanel();
 //        button_panel.setLayout(new BoxLayout(button_panel,BoxLayout.LINE_AXIS));
 //        button_panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        JButton button_delete = new JButton("Delete");
+    	JPanel panel12 = new JPanel(new GridLayout(2,1));
+    	button_delete = new JButton("Delete");
         String[] colHeads = { "Name", "Extension", "ID#" };
         Object[][] data = {
                 {"Gail","456715","5921214"},
@@ -99,8 +113,9 @@ class Panel2 extends JPanel {
         };
         JTable table = new JTable(data, colHeads);
         JScrollPane jsp = new JScrollPane(table);
-        add(jsp);
-        add(button_delete);
+        panel12.add(jsp);
+        panel12.add(button_delete);
+        add(panel12);
 //        button_panel.add(Box.createHorizontalGlue());
 //        button_panel.add(button_delete);
 //        button_panel.add(Box.createRigidArea(new Dimension(10,0)));
