d05025419f3f5b985ead66ea62ad5aba99119207
Calvin Kwee <calvink.ti@stu.untar.ac.id>
2016-12-07 10:20:47 +0700

    Update Multi_Threading.java

--- Multi_Threading.java
+++ Multi_Threading.java
@@ -58,7 +58,8 @@ class smail_func {
 		stmt.execute();
 		stmt.close();
 	}
-	public Vector<Vector<Object>> selectEmail(String id)throws SQLException{
+	public Vector<Vector<Object>> selectEmailSent(String id)throws SQLException{
+		stmt=conn.createStatement();
 		String query = "SELECT * FROM mail WHERE ID_Pengirim = '"+id+"'";
 		ResultSet rs=stmt.executeQuery(query);
 		Vector<Vector<Object>> datass = new Vector<Vector<Object>>();
@@ -74,7 +75,23 @@ class smail_func {
 		stmt.close();
 		return datass;
 	}
-
+	public Vector<Vector<Object>> selectEmailInbox(String id)throws SQLException{
+		stmt=conn.createStatement();
+		String query = "SELECT * FROM mail WHERE ID_Penerima = '"+id+"'";
+		ResultSet rs=stmt.executeQuery(query);
+		Vector<Vector<Object>> datass = new Vector<Vector<Object>>();
+		while(rs.next()){
+			Vector<Object> e = new Vector<Object>();
+			e.add(rs.getString("ID_Pengirim"));
+			e.add(rs.getString("Subjects"));
+			e.add(rs.getString("isi"));
+			e.add(rs.getString("E_status"));
+			e.add(rs.getString("Waktu_terima"));
+			datass.add(e);
+		}
+		stmt.close();
+		return datass;
+	}
 	public Vector<Object> Login(String email,String pass) throws SQLException{
 		stmt=conn.createStatement();
 		String query = "Select Nama, Email from users WHERE Email = '"+ email +"' And pass= '"+ pass +"'";
@@ -87,28 +104,17 @@ class smail_func {
 		stmt.close();
 		return ans;
 	}
-	public Vector<Vector<Object>> searchEmailInbox(String id)throws SQLException{
-		String query = "SELECT * FROM mail WHERE "
-				+ "(ID_Pengirim LIKE ?)or(ID_Pengirim LIKE ?)or(ID_Pengirim LIKE ?)or(ID_Pengirim LIKE ?)or"
-				+ "(Subject LIKE ?)or(Subject LIKE ?)or(Subject LIKE ?)or(Subject LIKE ?)or"
-				+ "(isi LIKE ?)or(isi LIKE ?)or(isi LIKE ?)or(isi LIKE ?)";
+	public Vector<Vector<Object>> searchEmail(String id)throws SQLException{
+		stmt = conn.createStatement();
 		String search1 = "%"+id;
 		String search2 = "%"+id+"%";
 		String search3 = id+"%";
 		String search4 = id;
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1, search1);
-		stmt.setString(2, search2);
-		stmt.setString(3, search3);
-		stmt.setString(4, search4);
-		stmt.setString(5, search1);
-		stmt.setString(6, search2);
-		stmt.setString(7, search3);
-		stmt.setString(8, search4);
-		stmt.setString(9, search1);
-		stmt.setString(10, search2);
-		stmt.setString(11, search3);
-		stmt.setString(12, search4);
+		String query = "SELECT * FROM mail WHERE "
+				+ "(ID_Pengirim LIKE '"+search1+"')or(ID_Pengirim LIKE '"+search2+"')or(ID_Pengirim LIKE '"+search3+"')or(ID_Pengirim LIKE '"+search4+"')or"
+				+ "(ID_Penerima LIKE '"+search1+"')or(ID_Penerima LIKE '"+search2+"')or(ID_Penerima LIKE '"+search3+"')or(ID_Penerima LIKE '"+search4+"')or"
+				+ "(Subject LIKE '"+search1+"')or(Subject LIKE '"+search2+"')or(Subject LIKE '"+search3+"')or(Subject LIKE '"+search4+"')or"
+				+ "(isi LIKE '"+search1+"')or(isi LIKE '"+search2+"')or(isi LIKE '"+search3+"')or(isi LIKE '"+search4+"')";
 		ResultSet rs=stmt.executeQuery(query);
 		System.out.println("");
 		Vector<Vector<Object>> datass = new Vector<Vector<Object>>();
@@ -124,49 +130,17 @@ class smail_func {
 		stmt.close();
 		return datass;
 	}
-	public Vector<Vector<Object>> searchEmailSent(String id)throws SQLException{
-		String query = "SELECT * FROM mail WHERE "
-				+ "(ID_Penerima LIKE ?)or(ID_Penerima LIKE ?)or(ID_Penerima LIKE ?)or(ID_Penerima LIKE ?)or"
-				+ "(Subject LIKE ?)or(Subject LIKE ?)or(Subject LIKE ?)or(Subject LIKE ?)or"
-				+ "(isi LIKE ?)or(isi LIKE ?)or(isi LIKE ?)or(isi LIKE ?)";
-		String search1 = "%"+id;
-		String search2 = "%"+id+"%";
-		String search3 = id+"%";
-		String search4 = id;
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1, search1);
-		stmt.setString(2, search2);
-		stmt.setString(3, search3);
-		stmt.setString(4, search4);
-		stmt.setString(5, search1);
-		stmt.setString(6, search2);
-		stmt.setString(7, search3);
-		stmt.setString(8, search4);
-		stmt.setString(9, search1);
-		stmt.setString(10, search2);
-		stmt.setString(11, search3);
-		stmt.setString(12, search4);
-		ResultSet rs=stmt.executeQuery(query);
-		System.out.println("");
-		Vector<Vector<Object>> datass = new Vector<Vector<Object>>();
-		while(rs.next()){
-			Vector<Object> e = new Vector<Object>();
-			e.add(rs.getString("ID_Penerima"));
-			e.add(rs.getString("Subjects"));
-			e.add(rs.getString("isi"));
-			e.add(rs.getString("E_status"));
-			e.add(rs.getString("Waktu_terima"));
-			datass.add(e);
-		}
-		stmt.close();
-		return datass;
-	}
 	public void hapus(String ID_Penerima,String Subjects,String isi,String ID_Pengirim) throws SQLException{
 		stmt=conn.createStatement();
 		String query = "DELETE FROM mail Where ID_Pengirim = '"+ID_Pengirim+"' and ID_Penerima = '"+ID_Penerima+"' and Subjects = '"+Subjects+"' and isi = '"+isi+"'";
 		stmt.execute(query);
 	}
-	//fungsi read
+	public void reads(String id)throws SQLException{
+		stmt=conn.createStatement();
+		String query = "UPDATE users SET E_Status = read WHERE Email = '"+id+"'";
+		stmt.execute(query);
+		stmt.close();
+	}
 	//fungsi verifikasi email yang dituju
 	public static void main(String[] args){
 	}
