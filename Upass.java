c4eda465b50d41c9872e5b425af2b3c83fb88c50
Calvin Kwee <calvink.ti@stu.untar.ac.id>
2016-12-07 13:13:00 +0700

    Update Upass.java

--- Upass.java
+++ Upass.java
@@ -23,9 +23,9 @@ public class Upass extends JFrame implements ActionListener{
 		setTitle("Password Change");
         setSize(420, 195);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        JPanel mainpanel= new JPanel(new GridLayout(5,1));
-        JPanel panel1= new JPanel(new GridLayout(1,2));
-        JPanel panel2= new JPanel(new GridLayout(1,2));
+        JPanel mainpanel= new JPanel(new GridLayout(5,1,5,5));
+        JPanel panel1= new JPanel(new GridLayout(1,2,5,5));
+        JPanel panel2= new JPanel(new GridLayout(1,2,5,5));
         JLabel Password = new JLabel("New Password");
         JLabel Password2 = new JLabel("Confirm your password");
 		pass = new JPasswordField();
